644e963f76233c07c812708f03e783b7b7e658a6
Android Studio <devandroid@ort>
2022-05-09 10:08:40 -0300

    index02.js

---
+++ TP04/src/modules/matematica.js
@@ -0,0 +1,13 @@
+function sumar(n1, n2){
+    return n1+n2;
+}
+
+function restar(n1, n2) {
+    return n1 - n2;
+}
+
+const dividir = (n1, n2) => n1 / n2;
+
+const Pi = 3.1416;
+
+export { sumar, restar, dividir, Pi };
