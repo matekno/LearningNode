e22ffa5dd80c1701b1efb3043515a114cb984753
Android Studio <devandroid@ort>
2022-05-10 08:24:37 -0300

    mas o menos, index04.js

--- TP04/src/index04.js
+++ TP04/src/index04.js
@@ -1,10 +1,33 @@
 import fs from 'fs';
 //NO ANDA!
 
-let file = '../text.txt';
-fs.readFileSync('text.txt', (err, data) =>{
+let pathito = './TP04/text.txt';
+
+console.log("antes");
+fs.readFile(path, (err, data) => {
     if (err) {
         throw err;
     }
-    console.log(data);
+    console.log(data.toString());
+    // si usamos readFile, por default es async.
+    // pero es un async raro. Pensar esto como un then..
 });
+console.log('durante');
+// .then(() => console.log("despues"));
+
+
+// No termino de entender como funciona el readFileSync:
+
+// function ReadEsta(path){
+//     fs.readFileSync(path, (err, data)=>{
+//         if (err) {
+//             throw err;
+//         }
+//         else{
+//             return data.toString();
+//         }
+//     });    
+// }
+
+// console.log(ReadEsta(pathito));
+
