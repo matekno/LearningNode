bdac706d2edc56fe5fe2840baf75c5020cd1d0b3
Android Studio <devandroid@ort>
2022-05-10 08:40:56 -0300

    mejoras al index05.js

--- TP04/src/index05.js
+++ TP04/src/index05.js
@@ -1,8 +1,11 @@
 const url = new URL("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo");
-let datos = {
-    'host': url.host, 
-    'path': url.pathname,
-    'params': url.searchParams
+
+function GetDataFromURL(url) {
+    return {
+        'host': url.host, 
+        'path': url.pathname,
+        'params': url.searchParams
+    }
 }
 
-console.log(datos.params);
+console.log(GetDataFromURL(url));
